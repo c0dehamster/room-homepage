ee7abf3e7fd968319a8e1baf7398f51e47f90c77
Sergej Smelov <smelowsergey@gmail.com>
2023-11-17 19:31:02 +0500

    Style forward/back buttons (mobile)

--- src/components/ControlButtons.tsx
+++ src/components/ControlButtons.tsx
@@ -3,12 +3,12 @@ import iconAngleRight from "../assets/images/icon-angle-right.svg"
 
 export const ControlButtons = () => {
 	return (
-		<div>
-			<button>
+		<div className="control-buttons | row-start-2 row-end-3 col-start-2 col-end-3 grid grid-cols-2">
+			<button className="h-14 w-14 grid place-items-center bg-black hover:bg-very-dark-grey">
 				<img src={iconAngleLeft} alt="previous slide" />
 			</button>
 
-			<button>
+			<button className="h-14 w-14 grid place-items-center bg-black hover:bg-very-dark-grey">
 				<img src={iconAngleRight} alt="next slide" />
 			</button>
 		</div>

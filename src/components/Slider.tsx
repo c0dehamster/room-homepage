2b2dc13839337469daf66632c4968d050b60302c
Sergej Smelov <smelowsergey@gmail.com>
2023-12-13 19:42:37 +0500

    Add slides,

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,26 +1,28 @@
+import { motion } from "framer-motion"
+
+import data from "../assets/data.json"
+
 export const Slider = () => {
+	const slides = data.map((item) => {
+		return (
+			<li className="slide | w-1/3" key={item.id}>
+				<picture>
+					<source media="(min-width: 30rem)" srcSet={item.imageDesktop} />
+					<source media="(max-width: 29.9rem)" srcSet={item.imageMobile} />
+					<img src={item.imageDesktop} alt="" className="slide__image | w-full desktop:h-full object-cover" />
+				</picture>
+			</li>
+		)
+	})
 	return (
-		// Placeholder until the proper carousel is implemented
 		<div className="slider-wrapper | col-start-1 col-end-2 row-start-1 row-end-2 tablet:col-end-3 overflow-hidden">
-			<ul className="slider | w-full h-full">
-				<li className="slide |  w-full h-full">
-					<picture>
-						<source
-							media="(min-width: 30rem)"
-							srcSet="./images/desktop-image-hero-1.jpg"
-						/>
-						<source
-							media="(max-width: 29.9rem)"
-							srcSet="./images/mobile-image-hero-1.jpg"
-						/>
-						<img
-							src="./images/desktop-image-hero-1.jpg"
-							alt=""
-							className="slide__image | w-full desktop:h-full object-cover"
-						/>
-					</picture>
-				</li>
-			</ul>
+			<motion.ul
+				className="slider | w-[300%] h-full overflow-hidden flex"
+				animate={{ x: -100 }}
+				transition={{ ease: "easeOut", duration: 0.5 }}
+			>
+				{slides}
+			</motion.ul>
 		</div>
 	)
 }
